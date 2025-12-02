05e9c60d536c905998d168fb09cd12909e28dffc
Diggory Laycock <nospam@diggory.net>
2025-12-02 08:59:34 +0000

    Beginning of Day 2

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -1,25 +1,250 @@
-import Algorithms
+//  https://adventofcode.com/2025/day/1
 
-struct Day00: AdventDay {
+import Algorithms
+import Foundation
+
+package struct Day01: AdventDay {
+  let dayNumeral = "01"
+
   // Save your data in a corresponding text file in the `Data` directory.
   var data: String
 
-  // Splits input data into its component parts and convert from string.
-  var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
+  /// Load challenge data from file in the Data folder
+  func loadData() -> String? {
+    let moduleBundle = Bundle.module
+    if let dataFileURL = moduleBundle.url(forResource: "Data/Day\(dayNumeral)", withExtension: "txt") {
+      if let fileContents = try? String(contentsOf: dataFileURL) {
+        return fileContents
+      }
     }
+    return nil
   }
+
+  /// An instruction for the safe dial (either L or R and a distance)
+  struct DialInstruction: CustomStringConvertible {
+    enum Direction: Int {
+      case L = -1
+      case R = 1
+    }
+    let direction: Direction
+    let distance: Int
+    let originalDistance: Int
+
+    let dialMin = 0
+    let dialMax = 99
+
+    var ignoredWholeSpins = 0
+
+    var description: String {
+      return "Turn \(direction == .L ? "Left" : "Right") by \(distance)"
+    }
+
+    /// Initialise instruction from a string e.g. 'L15' or 'R1'
+    public init?(string: String) {
+      guard string.count > 1 && string.count > 0  else {
+        print("could not init DialInstruction - string wrong length")
+        return nil
+      }
+      let directionLetter = string.first!
+      guard directionLetter == "L" || directionLetter == "R" else {
+        print("could not init DialInstruction - direction letter not L/R")
+        return nil
+      }
+      let distanceAsString = string.dropFirst()
+      guard var distanceAsInt = Int(distanceAsString) else {
+        print("could not init DialInstruction - distance not a number")
+        return nil
+      }
+      originalDistance = distanceAsInt
+
+      direction = directionLetter == "L" ? .L : .R
+
+//      //  Sanitise the distance so that it's not over 99 (we can ignore multiple whole spins)
+//      while distanceAsInt > 99 {
+//        distanceAsInt -= 100
+//        ignoredWholeSpins += 1
+//      }
+
+      //  Sanitise the distance so that it's not over 99 (we can ignore multiple whole spins)
+      if distanceAsInt > 99 {
+        let multiples = distanceAsInt / 100
+        let remainder = distanceAsInt % 100
+        distanceAsInt = remainder
+        ignoredWholeSpins = multiples
+      }
+
+
+      distance = distanceAsInt
+    }
+
+  }
+
+
+  /// The Safe dial
+  package struct Dial: CustomStringConvertible {
+    let dialMin = 0
+    let dialMax = 99
+
+    var currentPosition: Int = 0
+
+    init(startingAt currentPosition: Int) {
+      self.currentPosition = currentPosition
+    }
+
+    public var description: String {
+      return "Dial Position: \(currentPosition)"
+    }
+
+    /// Calculates dial number after an instruction has been applied (short-hand - doesn't apply multiple spins)
+    func numberAfterExecuting(instruction: DialInstruction) -> Int {
+//      print("Executing dial spin: \(self) from \(currentDialNumber)")
+      var newDialIndex = currentPosition + (instruction.distance * instruction.direction.rawValue)
+      if newDialIndex < dialMin {
+        let overshoot = dialMin - newDialIndex
+//        print("overshoot by: \(overshoot)")
+        //  Fencepost
+        newDialIndex = dialMax - overshoot + 1
+      } else if newDialIndex > dialMax {
+        let overshoot = newDialIndex - dialMax
+//        print("overshoot by: \(overshoot)")
+        //  Fencepost
+        newDialIndex = dialMin + overshoot - 1
+      }
+//      print("Dial now points at: \(newDialIndex)")
+      precondition(newDialIndex >= dialMin && newDialIndex <= dialMax, "invalid dial number!: \(newDialIndex)")
+      return newDialIndex
+    }
+
+    /// Actually apply the instruction to the dial.
+    mutating func executeInstruction(instruction: DialInstruction) {
+      currentPosition = numberAfterExecuting(instruction: instruction)
+    }
+
+    /// Given an instruction, how many times did the dial click past zero?
+    func numTimesPastZero(forInstruction instruction: DialInstruction) -> Int {
+      let components = componentsForInstruction(instruction)
+      if components.remainder == 0 {
+        return components.wholeSpins
+      }
+      //  Check if we are going left from zero
+      if currentPosition == 0 && instruction.direction == .L {
+        return components.wholeSpins
+      }
+      //  See if we cross once using the just the remainder
+      let dialWidth = dialMax - dialMin
+      let remainingDialWidth = ( instruction.direction == .R ? (dialWidth - currentPosition) : (currentPosition)  )
+      if components.remainder >= remainingDialWidth {
+        return components.wholeSpins + 1
+      }
+      return components.wholeSpins
+//      return 0
+    }
+
+    //  How many times around the dial
+    struct DialComponents {
+      let wholeSpins: Int
+      let remainder: Int
+    }
+
+    /// Gives the number of times that the dial goes round, and also the remainder
+    func componentsForInstruction(_ instruction: DialInstruction) -> DialComponents {
+      let fullDistance = instruction.originalDistance
+      let dialWidth = dialMax - dialMin + 1
+
+      if fullDistance < dialWidth {
+        return DialComponents(wholeSpins: 0, remainder: fullDistance)
+      }
+
+      //  More than One whole spin.
+      let multiples = fullDistance / dialWidth
+      let remainder = fullDistance % dialWidth
+
+      return DialComponents(wholeSpins: multiples, remainder: remainder)
+    }
+
+  }
+
+
+
+
+  //MARK: - Answer functions
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    // Calculate the sum of the first set of input data
-    entities.first?.reduce(0, +) ?? 0
+    print("Day 1")
+
+    let spinR1 = Day01.DialInstruction(string: "R1")
+    print(spinR1!)
+
+    //  Load the data
+    guard let data = loadData() else {
+      print("could not load day's data from file...")
+      return 0
+    }
+    //    print(data)
+
+    //  Turn the data into instructions
+    let spinInstructions = data.split(separator: "\n")
+    let spins = spinInstructions.compactMap { DialInstruction(string: String($0)) }
+
+    //  The safe has a dial with only an arrow on it; around the dial are the numbers 0 through 99 in order
+    //  Because the dial is a circle, turning the dial left from 0 one click makes it point at 99. Similarly, turning the dial right from 99 one click makes it point at 0.
+    //  The dial starts by pointing at 50.
+
+    //  The actual password is the number of times the dial is left pointing at 0 after any rotation in the sequence.
+
+    let dialStart = 50
+    var numberOfTimesDialIsZero: Int = 0
+    var dial = Dial(startingAt: dialStart)
+
+    for spin in spins {
+      dial.executeInstruction(instruction: spin)
+      if dial.currentPosition == 0 {
+        numberOfTimesDialIsZero += 1
+      }
+    }
+    print("We hit Zero \(numberOfTimesDialIsZero) times.")
+
+    return numberOfTimesDialIsZero
   }
+
+
+
+
+
+
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
+
+    print("=======")
+    print("PART 2")
+    print("=======")
+
+
+    //  Load the data
+    guard let data = loadData() else {
+      print("could not load day's data from file...")
+      return 0
+    }
+
+    //  Turn the data into instructions
+    let spinInstructions = data.split(separator: "\n")
+    let spins = spinInstructions.compactMap { DialInstruction(string: String($0)) }
+    let dialStart = 50
+
+    var dial = Dial(startingAt: dialStart)
+    var numberOfTimesDialIsZero: Int = 0
+
+    for spin in spins {
+      numberOfTimesDialIsZero += dial.numTimesPastZero(forInstruction: spin)
+      dial.executeInstruction(instruction: spin)
+    }
+    print("We hit Zero \(numberOfTimesDialIsZero) times.")
+
+    return numberOfTimesDialIsZero
+
   }
+
+
 }
--- Tests/Day01.swift
+++ Tests/Day01.swift
@@ -1,36 +1,254 @@
 import Testing
 
+
 @testable import AdventOfCode
 
 // Make a copy of this file for every day to ensure the provided smoke tests
 // pass.
-struct Day00Tests {
-  // Smoke test data provided in the challenge question
-  let testData = """
-    1000
-    2000
-    3000
+struct Day01Tests {
 
-    4000
+  class DialTests {
 
-    5000
-    6000
+    let foo = "Hello"
+    let spinR0 = Day01.DialInstruction(string: "R0")!
+    let spinR1 = Day01.DialInstruction(string: "R1")!
+    let spinL1 = Day01.DialInstruction(string: "L1")!
+    let spinR10 = Day01.DialInstruction(string: "R10")!
+    let spinL10 = Day01.DialInstruction(string: "L10")!
+    let spinR50 = Day01.DialInstruction(string: "R50")!
+    let spinL50 = Day01.DialInstruction(string: "L50")!
+    let spinR99 = Day01.DialInstruction(string: "R99")!
+    let spinL99 = Day01.DialInstruction(string: "L99")!
+    let spinR100 = Day01.DialInstruction(string: "R100")!
+    let spinL100 = Day01.DialInstruction(string: "L100")!
+    let spinR123 = Day01.DialInstruction(string: "R123")!
+    let spinR150 = Day01.DialInstruction(string: "R150")!
+    let spinL150 = Day01.DialInstruction(string: "L150")!
+    let spinR250 = Day01.DialInstruction(string: "R250")!
+    let spinL250 = Day01.DialInstruction(string: "L250")!
+    let spinL350 = Day01.DialInstruction(string: "L350")!
 
-    7000
-    8000
-    9000
 
-    10000
+    let dialStartPosition50: Int = 50
+    var dial = Day01.Dial(startingAt: 50)
 
-    """
+    func resetDial() {
+      dial.currentPosition = dialStartPosition50
+    }
 
-  @Test func testPart1() async throws {
-    let challenge = Day00(data: testData)
-    #expect(String(describing: challenge.part1()) == "6000")
+    @Test("Initial State")
+    func initialState() {
+      #expect(dial.currentPosition == 50)
+    }
+
+    @Test("R1") func spinR1Test() {
+      resetDial()
+      #expect(dial.numberAfterExecuting(instruction: spinR1) == 51)
+    }
+
+    @Test("L1") func spinL1Test() {
+      resetDial()
+      #expect(dial.numberAfterExecuting(instruction: spinL1) == 49)
+    }
+
+    @Test("R50") func spinR50Test() {
+      resetDial()
+      #expect(dial.numberAfterExecuting(instruction: spinR50) == 00)
+    }
+
+    @Test("L50") func spinL50Test() {
+      resetDial()
+      #expect(dial.numberAfterExecuting(instruction: spinL50) == 00)
+    }
+
+    @Test("R100") func spinR100Test() {
+      resetDial()
+      #expect(dial.numberAfterExecuting(instruction: spinR100) == 50)
+    }
+
+    @Test("L100") func spinL100Test() {
+      resetDial()
+      #expect(dial.numberAfterExecuting(instruction: spinL100) == 50)
+    }
+
+    @Test("R99") func spinR99Test() {
+      resetDial()
+      #expect(dial.numberAfterExecuting(instruction: spinR99) == 49)
+    }
+
+    @Test("L99") func spinL99Test() {
+      resetDial()
+      #expect(dial.numberAfterExecuting(instruction: spinL99) == 51)
+    }
+
+    @Test("R150") func spinR150Test() {
+      resetDial()
+      #expect(dial.numberAfterExecuting(instruction: spinR150) == 00)
+    }
+
+    @Test("L150") func spinL150Test() {
+      resetDial()
+      #expect(dial.numberAfterExecuting(instruction: spinL150) == 00)
+    }
+
+    //  Testing DialComponents
+    @Test("Components D10") func componentsW11D10Test() {
+      resetDial()
+      let components = dial.componentsForInstruction(spinR10)
+      #expect(components.wholeSpins == 0)
+      #expect(components.remainder == 10)
+    }
+
+    @Test("Components D0") func componentsW11D00Test() {
+      resetDial()
+      let components = dial.componentsForInstruction(spinR0)
+      #expect(components.wholeSpins == 0)
+      #expect(components.remainder == 0)
+    }
+
+
+    @Test("Components D99") func componentsW11D99Test() {
+      resetDial()
+      let components = dial.componentsForInstruction(spinR99)
+      #expect(components.wholeSpins == 0)
+      #expect(components.remainder == 99)
+    }
+
+    @Test("Components D100") func componentsW11D100Test() {
+      resetDial()
+      let components = dial.componentsForInstruction(spinR100)
+      #expect(components.wholeSpins == 1)
+      #expect(components.remainder == 0)
+    }
+
+    @Test("Components D150") func componentsW11D150Test() {
+      resetDial()
+      let components = dial.componentsForInstruction(spinR150)
+      #expect(components.wholeSpins == 1)
+      #expect(components.remainder == 50)
+    }
+
+    @Test("Components D123") func componentsW11D123Test() {
+      resetDial()
+      let components = dial.componentsForInstruction(spinR123)
+      #expect(components.wholeSpins == 1)
+      #expect(components.remainder == 23)
+    }
+
+    @Test("Components D250") func componentsW11D250Test() {
+      resetDial()
+      let components = dial.componentsForInstruction(spinR250)
+      #expect(components.wholeSpins == 2)
+      #expect(components.remainder == 50)
+    }
+
+    //  Times Past Zero...
+    @Test("Times past 0: R10 from 50") func timesPastZeroR10From50() {
+      resetDial()
+      #expect(dial.numTimesPastZero(forInstruction: spinR10) == 00)
+    }
+
+    @Test("Times past 0: L10 from 50") func timesPastZeroL10From50() {
+      resetDial()
+      #expect(dial.numTimesPastZero(forInstruction: spinL10) == 00)
+    }
+
+    @Test("Times past 0: R50 from 50") func timesPastZeroR50From50() {
+      resetDial()
+      #expect(dial.numTimesPastZero(forInstruction: spinR50) == 1)
+    }
+
+    @Test("Times past 0: L50 from 50") func timesPastZeroL50From50() {
+      resetDial()
+      #expect(dial.numTimesPastZero(forInstruction: spinL50) == 1)
+    }
+
+    @Test("Times past 0: R100 from 50") func timesPastZeroR100From50() {
+      resetDial()
+      #expect(dial.numTimesPastZero(forInstruction: spinR100) == 1)
+    }
+
+    @Test("Times past 0: L100 from 50") func timesPastZeroL100From50() {
+      resetDial()
+      #expect(dial.numTimesPastZero(forInstruction: spinL100) == 1)
+    }
+
+    @Test("Times past 0: R150 from 50") func timesPastZeroR150From50() {
+      resetDial()
+      #expect(dial.numTimesPastZero(forInstruction: spinR150) == 2)
+    }
+
+    @Test("Times past 0: L150 from 50") func timesPastZeroL150From50() {
+      resetDial()
+      #expect(dial.numTimesPastZero(forInstruction: spinL150) == 2)
+    }
+
+
+
+
+    @Test("Times past 0: R50 from 0") func timesPastZeroR50From0() {
+      resetDial()
+      dial.currentPosition = 0
+      #expect(dial.numTimesPastZero(forInstruction: spinR50) == 0)
+    }
+
+    @Test("Times past 0: L50 from 0") func timesPastZeroL50From0() {
+      resetDial()
+      dial.currentPosition = 0
+      #expect(dial.numTimesPastZero(forInstruction: spinL50) == 0)
+    }
+
+    @Test("Times past 0: L1 from 1") func timesPastZeroL1From1() {
+      resetDial()
+      dial.currentPosition = 1
+      #expect(dial.numTimesPastZero(forInstruction: spinL1) == 1)
+    }
+
+    @Test("Times past 0: R1 from 1") func timesPastZeroR1From1() {
+      resetDial()
+      dial.currentPosition = 1
+      #expect(dial.numTimesPastZero(forInstruction: spinR1) == 0)
+    }
+
+    @Test("Times past 0: L1 from 99") func timesPastZeroL1From99() {
+      resetDial()
+      dial.currentPosition = 99
+      #expect(dial.numTimesPastZero(forInstruction: spinL1) == 0)
+    }
+
+    @Test("Times past 0: R1 from 99") func timesPastZeroR1From99() {
+      resetDial()
+      dial.currentPosition = 99
+      #expect(dial.numTimesPastZero(forInstruction: spinR1) == 1)
+    }
+
+
+    @Test("Times past 0: L100 from 0") func timesPastZeroL100From0() {
+      resetDial()
+      dial.currentPosition = 0
+      #expect(dial.numTimesPastZero(forInstruction: spinL100) == 1)
+    }
+
+    @Test("Times past 0: L150 from 0") func timesPastZeroL150From0() {
+      resetDial()
+      dial.currentPosition = 0
+      #expect(dial.numTimesPastZero(forInstruction: spinL150) == 1)
+    }
+
+    @Test("Times past 0: L250 from 0") func timesPastZeroL250From0() {
+      resetDial()
+      dial.currentPosition = 0
+      #expect(dial.numTimesPastZero(forInstruction: spinL250) == 2)
+    }
+
+    @Test("Times past 0: L350 from 0") func timesPastZeroL350From0() {
+      resetDial()
+      dial.currentPosition = 0
+      #expect(dial.numTimesPastZero(forInstruction: spinL350) == 3)
+    }
+
+
+
   }
 
-  @Test func testPart2() async throws {
-    let challenge = Day00(data: testData)
-    #expect(String(describing: challenge.part2()) == "32000")
-  }
 }
