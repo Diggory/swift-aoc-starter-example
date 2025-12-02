5e20e03b881ecf7e6d8da41923fbc2634f7aa421
Diggory Laycock <nospam@diggory.net>
2025-12-02 12:15:16 +0000

    Day 2 done.

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -3,6 +3,46 @@
 import Algorithms
 import Foundation
 
+extension Int {
+
+  ///  Number of digits
+  var length: Int {
+    return String(self).count
+  }
+
+  ///   Is the number a double number : e.g. 1234512345
+  func hasBeenElfed() -> Bool {
+    if !self.length.isMultiple(of: 2) {
+      return false
+    }
+    let str = String(self)
+    let front = String(str.prefix(self.length / 2))
+    let back = str.suffix(self.length / 2)
+    return front == back
+  }
+
+  ///   Is the number at least two copies of a sequence?
+  func hasBeenElfed2() -> Bool {
+    let str = String(self)
+
+    let maxSeqLength = self.length / 2
+
+//    if !self.length.isMultiple(of: 2) {
+//      return false
+//    }
+    var seqLength = 1
+    while seqLength <= maxSeqLength {
+      let chunks = str.chunks(ofCount: seqLength)
+      if chunks.allSatisfy({ $0 == chunks.first! }) {
+        return true
+      }
+      seqLength+=1
+    }
+    return false
+  }
+
+}
+
 package struct Day02: AdventDay {
   let dayNumeral = "02"
 
@@ -10,17 +50,36 @@ package struct Day02: AdventDay {
   var data: String
 
   /// Load challenge data from file in the Data folder
-  func loadData() -> String? {
+  func loadData() -> [ClosedRange<Int>]? {
+    var ranges = [ClosedRange<Int>]()
     let moduleBundle = Bundle.module
-    if let dataFileURL = moduleBundle.url(forResource: "Data/Day\(dayNumeral)", withExtension: "txt") {
-      if let fileContents = try? String(contentsOf: dataFileURL) {
-        return fileContents
+
+    guard let dataFileURL = moduleBundle.url(forResource: "Data/Day\(dayNumeral)", withExtension: "txt") else {
+      print("Unable to form URL for day data file name")
+      return nil
+    }
+    guard let fileContents = try? String(contentsOf: dataFileURL) else {
+      print("Unable to read file at URL: \(dataFileURL)")
+      return nil
+    }
+    //  Remove trailing newline
+    let noNewlines = fileContents.replacingOccurrences(of: "\n", with: "")
+    let rangePairsAsStrings = noNewlines.split(separator: ",").map {$0.split(separator: "-")}
+    for rangeAsStringPair in rangePairsAsStrings {
+      if let start = Int(String(rangeAsStringPair[0])), let end = Int(String(rangeAsStringPair[1])) {
+        let range = start...end
+        print(range)
+        ranges.append(range)
+      } else {
+        print("Unable to extract range from: \(rangeAsStringPair)")
       }
     }
-    return nil
+    return ranges
   }
 
+  func foo() {
 
+  }
 
 
 
@@ -34,7 +93,28 @@ package struct Day02: AdventDay {
     print("PART 1")
     print("=======")
 
-    return 0
+    guard let ranges = loadData() else {
+      print("unable to load data from file...");
+      return 0
+    }
+    //    print("loaded ranges: \(ranges)")
+
+    //  Since the young Elf was just doing silly patterns, you can find the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice. So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
+    //  None of the numbers have leading zeroes; 0101 isn't an ID at all. (101 is a valid ID that you would ignore.)
+
+    var countOfElfedIDs = 0
+    var sumOfElfedIDs = 0
+    for range in ranges {
+      for num in range {
+        if num.hasBeenElfed() {
+          print("elfed: \(num)")
+          countOfElfedIDs+=1
+          sumOfElfedIDs+=num
+        }
+      }
+    }
+
+    return sumOfElfedIDs
   }
 
 
@@ -50,14 +130,23 @@ package struct Day02: AdventDay {
     print("PART 2")
     print("=======")
 
-
-    //  Load the data
-    guard let data = loadData() else {
-      print("could not load day's data from file...")
+    guard let ranges = loadData() else {
+      print("unable to load data from file...");
       return 0
     }
 
-    return 0
+    var countOfElfedIDs = 0
+    var sumOfElfedIDs = 0
+    for range in ranges {
+      for num in range {
+        if num.hasBeenElfed2() {
+//          print("elfed2: \(num)")
+          countOfElfedIDs+=1
+          sumOfElfedIDs+=num
+        }
+      }
+    }
+    return sumOfElfedIDs
   }
 
 
